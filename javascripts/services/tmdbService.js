3638ff4e0af650875d7fcd7e5347e561e1278068
Waamber <amberastuart@aol.com>
2017-11-14 20:33:53 -0600

    movie cards with images printing to dom

--- javascripts/services/tmdbService.js
+++ javascripts/services/tmdbService.js
@@ -5,6 +5,11 @@ app.service("tmdbService", function ($http, TMDB_CONFIG) {
     return $http.get(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_CONFIG}&language=en-US&page=1&include_adult=false
 &query=${query}`);
   };
-  return { searchMovies };
+
+  const tmdbConfiguration = () => {
+    return $http.get(`https://api.themoviedb.org/3/configuration?api_key=${TMDB_CONFIG}`);
+  };
+
+  return { searchMovies, tmdbConfiguration };
 });
 
