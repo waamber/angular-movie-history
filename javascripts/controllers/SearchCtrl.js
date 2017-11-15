14ef7f8ded55cc2fb8a5efad7817d41054320f73
Waamber <amberastuart@aol.com>
2017-11-14 19:12:54 -0600

    added search event

--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -1,5 +1,15 @@
 'use strict';
 
-app.controller("SearchCtrl", function ($scope) {
-  $scope.controller = "SearchCtrl";
+
+app.controller("SearchCtrl", function ($scope, tmdbService) {
+
+  $scope.enterPush = (event) => {
+    if (event.keyCode === 13) {
+      tmdbService.searchMovies(event.target.value).then((results) => {
+        console.log("Search results", results.data.results);
+      }).catch((error) => {
+        console.log("Error in SearchCtrl", error);
+      });
+    }
+  };
 });
