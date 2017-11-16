729c32af1e97d8aa58504aa5c5b17b6221309fee
Waamber <amberastuart@aol.com>
2017-11-15 20:29:53 -0600

    added wishlist

--- javascripts/controllers/WishlistCtrl.js
+++ javascripts/controllers/WishlistCtrl.js
@@ -1,5 +1,9 @@
 'use strict';
 
-app.controller("WishlistCtrl", function ($scope) {
-  $scope.controller = "WishlistCtrl";
+app.controller("WishlistCtrl", function ($rootScope, $scope, MovieService) {
+  MovieService.getWishListMovies($rootScope.uid).then((results) => {
+    $scope.movies = results;
+  }).catch((error) => {
+    console.log("Error in MovieService", error);
+  });
 });
