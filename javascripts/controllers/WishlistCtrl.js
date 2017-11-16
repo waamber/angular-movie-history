77e436420e3f900b66b81f53e4546a5ce8451b2a
Waamber <amberastuart@aol.com>
2017-11-15 21:30:19 -0600

    delete wishlist and rated movies from firebase and dom

--- javascripts/controllers/WishlistCtrl.js
+++ javascripts/controllers/WishlistCtrl.js
@@ -1,9 +1,22 @@
 'use strict';
 
 app.controller("WishlistCtrl", function ($rootScope, $scope, MovieService) {
-  MovieService.getWishListMovies($rootScope.uid).then((results) => {
-    $scope.movies = results;
-  }).catch((error) => {
-    console.log("Error in MovieService", error);
-  });
+
+  const getMovies = () => {
+    MovieService.getWishListMovies($rootScope.uid).then((results) => {
+      $scope.movies = results;
+    }).catch((error) => {
+      console.log("Error in MovieService", error);
+    });
+  };
+
+  getMovies();
+
+  $scope.deleteMovie = (movieId) => {
+    MovieService.deleteMovie(movieId).then((result) => {
+      getMovies(); //page and movies reload after delete
+    }).catch((error) => {
+      console.log("Error in deleteMovie", error);
+    });
+  };
 });
--- javascripts/services/MovieService.js
+++ javascripts/services/MovieService.js
@@ -42,7 +42,11 @@ app.service("MovieService", function ($http, $q, FIREBASE_CONFIG) {
     return $http.post(`${FIREBASE_CONFIG.databaseURL}/movies.json`, JSON.stringify(newMovie));
   };
 
-  return { getRatedMovies, getWishListMovies, postNewMovie };
+  const deleteMovie = (movieId) => {
+    return $http.delete(`${FIREBASE_CONFIG.databaseURL}/movies/${movieId}.json`);
+  };
+
+  return { deleteMovie, getRatedMovies, getWishListMovies, postNewMovie };
 });
 
 
